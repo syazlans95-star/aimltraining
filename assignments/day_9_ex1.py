a1963a7497c1fb9871a07f988f217c193cab2e8c
syazlans95-star <syazlans95@gmail.com>
2025-10-16 16:31:59 +0800

    Welcome notebook

--- assignments/day_9_ex1.py
+++ assignments/day_9_ex1.py
@@ -1,18 +1,18 @@
 # Write a program using function to find greatest of three numbers entered by 
 
-def find_greatest_number(num1, num2, num3,):
-    if num1 > num2 and num1 > num3:
-        return num1
-    elif num2 > num1 and num2 > num3:
-        return num2
-    else:
-        return num3
+#def find_greatest_number(num1, num2, num3,):
+#    if num1 > num2 and num1 > num3:
+#        return num1
+#    elif num2 > num1 and num2 > num3:
+#        return num2
+#    else:
+#        return num3
 
-num1=int(input("Enter first number"))
-num2=int(input("Enter second number"))
-num3=int(input("Enter third number"))
+#num1=int(input("Enter first number"))
+#num2=int(input("Enter second number"))
+#num3=int(input("Enter third number"))
 
-result = find_greatest_number(num1, num2, num3)
+#result = find_greatest_number(num1, num2, num3)
 
 
-print(f"The greatest number is {result}")
+#print(f"The greatest number is {result}")
